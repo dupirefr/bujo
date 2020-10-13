223d850deb7e68dd0544693a966dd82118191a7a
fdpro <dupire.francois+pro@gmail.com>
2020-10-13 21:58:24 +0200

    [#20] Fixed compilation bug created by previous hotfix

--- lib/bujo.rb
+++ lib/bujo.rb
@@ -6,13 +6,15 @@ require 'bujo/plugins/init_plugin'
 require 'bujo/templates/template_renderer'
 
 def parse(args = [])
+  template_renderer = Templates::TemplateRenderer.new
+
   if File.exists?(Configuration::Structure.local_path("bujo.yaml"))
     configuration = Configuration::Configuration.load
-    plugin_repository = Plugins::PluginRepository.new(configuration)
+    plugin_repository = Plugins::PluginRepository.new(configuration, template_renderer)
     Plugins::PluginRegister.new(plugin_repository.find_all).parse(args)
   else
     Plugins::PluginRegister
-        .new([Plugins::InitPlugin.new({template_renderer: Templates::TemplateRenderer.new})])
+        .new([Plugins::InitPlugin.new({template_renderer: template_renderer})])
         .parse(args)
   end
 end
